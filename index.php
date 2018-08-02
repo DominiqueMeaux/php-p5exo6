b0c82acb695495422a8fb6118b240ea822156d90
dominiquem <dominique.meaux@gmail.com>
2018-08-02 16:18:32 +0200

    exo6

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>p5exo6</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $departements = array('02' => 'Aisne','59' => 'Nord', '60' => 'Oise', '80' => 'Somme', '62' => 'Pas-de-Calais');
+          echo $departements['59'];
+      ?>
+    </p>
+  </body>
+</html>
